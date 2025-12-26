c251cd1e8d9959bb154d889f04566e90b97775b5
eric1sphang-svg <eric1s.phang@gmail.com>
2025-12-26 19:15:48 +0700

    Initialization

---
+++ Player.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Player {
+    ArrayList<Player> player = new ArrayList<>();
+
+    
+}
---
+++ Team.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Team {
+    ArrayList<Team> team = new ArrayList<>();
+
+    
+}
